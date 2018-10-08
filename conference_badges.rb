376af9de4fcc0f0b31f40423e5594db590293ab9
Sophie Gu <sophieqgu@gmail.com>
2018-10-08 00:19:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end 
+
+def batch_badge_creater(name_array)
+  name_array.each do |name|
+    badge_maker(name)
+  end 
+end 
+
+def assign_rooms(speaker_list)
+  speaker_list.each_with_index do |speaker, index|
+    puts "Hello, #{speaker}! You'll be assigned to room hash(speaker)"
