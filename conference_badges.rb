c8a501672221ac6c16a04ed4a98053b720d00796
Sophie Gu <sophieqgu@gmail.com>
2018-10-08 01:05:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(name_array)
+  name_array.collect do |name|
+    badge_maker(name)
+  end 
+end 
+
+def assign_rooms(speaker_list)
+  speaker_list.collect do |speaker|
+    "Hello, #{speaker}! You'll be assigned to room #{speaker_list.index(speaker) + 1}!"
+  end 
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |name|
+    puts name 
+  end 
+  assign_rooms(attendees). each do |room|
+    puts room 
+  end 
+end 
