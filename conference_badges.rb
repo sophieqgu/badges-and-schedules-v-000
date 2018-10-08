3fca2fbe9a48453afa8922a16ea842a2dd301317
Sophie Gu <sophieqgu@gmail.com>
2018-10-08 00:38:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,16 @@
 # Write your code here.
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end 
 
 def batch_badge_creator(name_array)
-  name_array.each do |name|
+  name_array.collect do |name|
     badge_maker(name)
   end 
 end 
 
 def assign_rooms(speaker_list)
-  speaker_list.each_with_index do |speaker, index|
-    puts "Hello, #{speaker}! You'll be assigned to room #{index}"
+  speaker_list.collect do |speaker|
+    puts "Hello, #{speaker}! You'll be assigned to room #{speaker_list.index}"
   end 
 end 
