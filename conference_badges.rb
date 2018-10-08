22d6ba200fad76ea24bd35fb08efd6d38340a0f2
Sophie Gu <sophieqgu@gmail.com>
2018-10-08 00:20:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,4 +11,6 @@ end
 
 def assign_rooms(speaker_list)
   speaker_list.each_with_index do |speaker, index|
-    puts "Hello, #{speaker}! You'll be assigned to room hash(speaker)"
+    puts "Hello, #{speaker}! You'll be assigned to room #{index})"
+  end 
+end 
